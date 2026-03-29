5ef637d185d369efb87f10ee3bfe6e24b0eb31a8
chandani56 <gunutiwari56@gmail.com>
2026-03-29 13:42:41 +0530

    Add files via upload

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const bookingController = require('../controllers/bookingController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.use(authController.protect);
+
+router.post('/', bookingController.createBooking);
+
+module.exports = router;
